71554125dc1e920340d4fa54f05d15201f84228f
Daniel Regan <d.regan2002@gmail.com>
2020-09-23 16:43:11 +1200

    Worked on index, register, account and channel pages. Fixed miner problems throughout them

--- classes/Router.js
+++ classes/Router.js
@@ -12,8 +12,8 @@ class Router{
 
     constructor(){
         this.setVariables();
-        this.addBaseRoutes();
         this.addControllers();
+        this.addBaseRoutes();
         this.handle404s();
         this.handleErrors();
     }
@@ -23,7 +23,20 @@ class Router{
      * to local values
      */
     setVariables(){
-        AraDTApp.use(function(request, response, next) {
+
+        AraDTApp.use(async (request, response, next) => {
+
+            console.log("################# Session Data #####################");
+            console.log(request.session);
+            // Check if user logged in for this session
+            if (request.session.user) {
+                response.locals.user = request.session.user;
+                response.locals.loggedin = true;
+            }
+
+            response.locals.channels = {};
+            await this.fetchAllChannelsData(response);
+            
             if (request.session.errors) {
                 response.locals.errors = request.session.errors;
             }
@@ -39,21 +52,18 @@ class Router{
      */
     addBaseRoutes() {
         AraDTApp.get('/', this.index);
-        AraDTApp.get('/register', this.signup);
-
     }
 
-    signup(request, response){
-        response.render('register');
-    }
 
-       /**
+    /**
      * Add controllers for key models, 
      * e.g. Users, Channels, Messages
      */
     addControllers() {
-        var userController = new UserController();
         var channelController = new ChannelController();
+        var userController = new UserController();
+        channelController.addRoutes();
+        userController.addRoutes();
     }
 
     // Renders home page ./views/index.ejs
@@ -73,6 +83,7 @@ class Router{
         
         //  error handler
         AraDTApp.use(function(error, request, response, next) {
+            
             if (error) {
                 console.log('Error', error);
             }
@@ -84,6 +95,12 @@ class Router{
             response.status(error.status || 500);
             response.render('error');
         });
+    }
+    
+    fetchAllChannelsData = async (response) => {
+        response.locals.channels.all = await AraDTChannelModel.getAllChannels();
+        console.log("################# All Channels Data #####################");
+        console.log(response.locals.channels.all);
     }
 
 }
